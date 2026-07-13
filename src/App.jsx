bf368c92d857e67bf096a4c8e2883ab8e7d35ccb
Anurag3215 <anuragaacc@gmail.com>
2026-07-13 10:08:34 +0530

    Add Donor completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import AddDonor from './components/AddDonor'
 
 function App() {
@@ -12,15 +11,7 @@ function App() {
   return (
     <>
       
-<BrowserRouter>
-
-<Routes>
-
-<Route path='/' element={<AddDonor/>}/>
-
-</Routes>
-
-</BrowserRouter>
+      <AddDonor/>
 
     </>
   )
